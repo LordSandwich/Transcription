4de95be830e1149a1747b690914969f6394cab69
LordSandwich <jknight77@gmail.com>
2025-05-08 12:56:51 +1000

    Calendar and TimePicker have been setup and working as intended

--- transcription.py
+++ transcription.py
@@ -1,8 +1,25 @@
 import tkinter as tk
+import datetime as dt
 from tkinter import ttk
+from tkcalendar import Calendar, DateEntry
+from tktimepicker import AnalogPicker, AnalogThemes, constants
 
 root = tk.Tk()
 root.title("On Demand to Transcription")
+
+
+
+#########
+#
+#   Up to generating DateTime that matches the GUI at start of app
+#   setup a date time and use current day/month/year but set time
+#   to 00:59 as that's what the GUI shows
+#
+#########
+
+
+# Create a DateTime instance.
+#selectedDateTime = dt.datetime()
 
 def minutesToSeconds(minutes):
     # Returns an integer
@@ -11,19 +28,48 @@ def minutesToSeconds(minutes):
     return int(minutes) * 60
 
 def genURL():
+    currentDT = set_datetime(cal,tp)
+    
     baseURL = "https://rrr.org.au/"
     lengthOption = "&l="
-    dateTime = yearVar.get() + monthVar.get() + dayVar.get() + \
-               hourVar.get() + minuteVar.get() + secondVar.get()
-    newVar = baseURL + dateTime + lengthOption + \
+    dtStrFormat = '%Y%m%d%H%M%S'
+    dateTimeStr = currentDT.strftime(dtStrFormat)    
+    newVar = baseURL + dateTimeStr + lengthOption + \
              str(minutesToSeconds(lengthVar.get()))
     urlVar.set(newVar)
 
 def copyToClipboard():
+    genURL()
     root.clipboard_clear()
     root.clipboard_append(urlVar.get())
-    #root.update()
 
+def convert12to24(hour, am_pm):
+    if am_pm == 'AM' and hour == 12:
+        hour = 0
+        return hour
+    if am_pm == 'PM':
+        if hour == 12:
+            return hour
+        hour = int(hour) + 12
+    return hour
+
+def print_datetime(selectedDateTime):
+    dtFormatString = '%Y-%m-%d-%H-%M-%S'
+    print(selectedDateTime.strftime(dtFormatString))
+
+def set_datetime(cal, tp):
+    #unpacking time tuple
+    hour, minute, am_pm = tp.time()
+
+    hour = convert12to24(hour, am_pm)
+    tpTime = dt.time(hour=hour, minute=minute)
+    calDate = cal.selection_get()
+    selectedDateTime = dt.datetime.combine(calDate, tpTime)
+
+    print_datetime(selectedDateTime)
+    return selectedDateTime
+    
+# Setup Frames
 topFrame = ttk.Frame(root)
 topFrame['padding'] = 5
 
@@ -37,30 +83,6 @@ titleLabel = ttk.Label(topFrame)
 titleLabel['text'] = "On Demand to Transcription helper"
 
 # Create selectors frame widgets
-yearLabel = ttk.Label(selectorsFrame)
-yearLabel['text'] = "Year:"
-
-yearVar = tk.StringVar(value='2025')
-yearEntry = ttk.Entry(selectorsFrame)
-yearEntry['textvariable'] = yearVar
-yearEntry['width'] = 8
-
-
-monthLabel = ttk.Label(selectorsFrame)
-monthLabel['text'] = "Month:"
-
-monthVar = tk.StringVar()
-monthEntry = ttk.Entry(selectorsFrame)
-monthEntry['textvariable'] = monthVar
-monthEntry['width'] = 8
-
-dayLabel = ttk.Label(selectorsFrame)
-dayLabel['text'] = "Day:"
-
-dayVar = tk.StringVar()
-dayEntry = ttk.Entry(selectorsFrame)
-dayEntry['textvariable'] = dayVar
-dayEntry['width'] = 8
 
 hourLabel = ttk.Label(selectorsFrame)
 hourLabel['text'] = "Hour:"
@@ -101,41 +123,54 @@ urlEntry['textvariable'] = urlVar
 urlEntry['width'] = 55
 urlVar.set("https://rrr.org.au/20250423111500&l=300")
 
-generateBtn = ttk.Button(urlFrame, text='Generate', command=genURL)
+generateBtn = ttk.Button(
+    urlFrame,
+    text='Generate',
+    command=genURL
+)
+
 copyBtn = ttk.Button(
     urlFrame,
     text='Copy',
     command=copyToClipboard
 )
 
+# Setup calendar widget
+cal = Calendar(selectorsFrame,
+               font="Arial 14",
+               selectmode = 'day',
+               locale = 'en_AU',
+               date_pattern = 'dd/mm/y'
+               )
+
+# Setup time picker widget
+tp = AnalogPicker(selectorsFrame, type=constants.HOURS12)
+tp_theme = AnalogThemes(tp)
+tp_theme.setDracula()
+			
 # Arrange frames
 topFrame.grid(column=0,row=0)
 selectorsFrame.grid(column=0,row=1)
 urlFrame.grid(column=0,row=2)
 
-# Arrange widgets
+## Arrange widgets
 # Arrange top widgets
 titleLabel.grid(column=0,row=0)
 
 # Arrange selector widgets
-yearLabel.grid(column=0, row=0, sticky='e', padx=2, pady=2)
-yearEntry.grid(column=1, row=0, sticky='w', padx=2, pady=2)
-monthLabel.grid(column=0, row=1, sticky='e', padx=2, pady=2)
-monthEntry.grid(column=1, row=1, sticky='w', padx=2, pady=2)
-dayLabel.grid(column=0, row=2, sticky='e', padx=2, pady=2)
-dayEntry.grid(column=1, row=2, sticky='w', padx=2, pady=2)
-hourLabel.grid(column=2, row=0, sticky='e', padx=2, pady=2)
-hourEntry.grid(column=3, row=0, sticky='w', padx=2, pady=2)
-minuteLabel.grid(column=2, row=1, sticky='e', padx=2, pady=2)
-minuteEntry.grid(column=3, row=1, sticky='w', padx=2, pady=2)
-secondLabel.grid(column=2, row=2, sticky='e', padx=2, pady=2)
-secondEntry.grid(column=3, row=2, sticky='w', padx=2, pady=2)
-lengthLabel.grid(column=1, row=3, columnspan=2, sticky='e', padx=2, pady=2)
-lengthEntry.grid(column=3, row=3, sticky='w', padx=2, pady=2)
+cal.grid(column=0, row=0, sticky='e', padx=2, pady=2)
+tp.grid(column=1, row=0, sticky='e', padx=2, pady=2)
+
+lengthLabel.grid(column=0, row=2, sticky='e', padx=2, pady=2)
+lengthEntry.grid(column=1, row=2, sticky='w', padx=2, pady=2)
 
 # Arrange URL widgets
 generateBtn.grid(column=0, row=0)
 copyBtn.grid(column=1, row=0)
 urlEntry.grid(column=0, row=1, columnspan=2)
+
+# Set initial DateTime
+#selectedDateTime = set_datetime(cal, tp)
+
 
 root.mainloop()
