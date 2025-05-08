25a91cfbb0320fcb33604dcb7fd108f5ca7a374a
LordSandwich <jknight77@gmail.com>
2025-05-08 17:46:21 +1000

    another crack

--- transcription.py
+++ transcription.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import datetime as dt
 from tkinter import ttk
+from tkinter import *
 from tkcalendar import Calendar, DateEntry
 from tktimepicker import AnalogPicker, AnalogThemes, constants
 
